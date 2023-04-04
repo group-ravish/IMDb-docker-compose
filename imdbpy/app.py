a55e5f7362aab0d22f8d870044dbd8111371138a
Guru <guru@Gurus-MacBook-Pro.local>
2023-04-04 19:32:25 +0530

    MYP-11: Added '' as the rating argument to certain APIs

--- imdbpy/app.py
+++ imdbpy/app.py
@@ -97,7 +97,7 @@ def getTopTV(rating):
 
 @app.route("/")
 def home():
-    return ('Welcome to my favorite movies list!')
+    return ('Welcome to our movie database!')
 
 @app.route("/movies/<movie>" , methods=['GET'])
 def get(movie):
@@ -129,7 +129,7 @@ def cast():
 
 @app.route("/movies/popular100", methods=['GET'])
 def top():
-    return getPopularMovies()
+    return getPopularMovies('')
 
 @app.route("/movies/top250", methods=['GET'])
 def top250():
@@ -137,15 +137,15 @@ def top250():
 
 @app.route("/movies/bottom100", methods=['GET'])
 def bottom100():
-    return getBottom100()
+    return getBottom100('')
 
 @app.route("/movies/ind250", methods=['GET'])
 def indian250():
-    return getTop250Indian()
+    return getTop250Indian('')
 
 @app.route("/movies/toptv", methods=['GET'])
 def top250TV():
-    return getTopTV()
+    return getTopTV('')
 
 
 if __name__ == '__main__':
